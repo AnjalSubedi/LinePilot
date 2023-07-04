631408a20e3c5acf9a134ed95a66301fb4584cd0
Anjalsubedi <anjalsubedi170@gmail.com>
2023-07-04 09:43:52 +0545

    PWM added

--- LinePilot.ino
+++ LinePilot.ino
@@ -1,35 +1,33 @@
-
 const int leftSensorPin = 2;
 const int centerSensorPin = 3;
 const int rightSensorPin = 4;
-const int motorPin3 = 10;
-const int motorPin4 = 11;
-const int motorPin1 = 5;
-const int motorPin2 = 6;
+const int motorPin1 = 5;//to digital pin 5
+const int motorPin2 = 6;//to digital pin 6 
 const int trigPin = 7;
-const int echoPin = 8;
+const int echoPin = 9;
+const int enpin = 11; //both ena and enb pin to digital pin 11
 long distance;
 long duration;
-
 void setup() {
   Serial.begin(9600);
+  pinMode(enpin,OUTPUT);
   pinMode(leftSensorPin, INPUT);
   pinMode(centerSensorPin, INPUT);
   pinMode(rightSensorPin, INPUT);
    pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(motorPin3 , OUTPUT);
-  pinMode(motorPin4 , OUTPUT);
+  pinMode(motorPin1 , OUTPUT);
+  pinMode(motorPin2 , OUTPUT);
   
 }
-
 void loop() {
-  
+  analogWrite(enpin,190);
   int leftSensorValue = digitalRead(leftSensorPin);
   int centerSensorValue = digitalRead(centerSensorPin);
   int rightSensorValue = digitalRead(rightSensorPin);
   int isLeft = leftSensorValue;
   int isRight = rightSensorValue;
+  int iscenter = centerSensorValue;
 digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -38,35 +36,33 @@ digitalWrite(trigPin, LOW);
   
   
  duration = pulseIn(echoPin , HIGH);
- distance = duration*0.034 / 2;
- Serial.print("Obstacle Distance: ");
-  Serial.println(distance);
+ distance = duration*0.0322 /2;
+
  if(distance<20){
-  Serial.println("Object is detected!!");
-  digitalWrite(motorPin3,LOW);
-  digitalWrite(motorPin4,LOW);
+ Serial.println("Object is detected,stop!!");
+  digitalWrite(motorPin1,LOW);
+  digitalWrite(motorPin2,LOW);
  }
  else{
-  if (isLeft==1 && isRight==0) {
-    
-    digitalWrite(motorPin3,HIGH);
-     digitalWrite(motorPin4,LOW);
-       digitalWrite(motorPin1,LOW);
-     digitalWrite(motorPin4,HIGH);
-  } else if (isLeft==0 && isRight==1) {
-   
-    digitalWrite(motorPin3,LOW);
-    digitalWrite(motorPin4,HIGH);
-    digitalWrite(motorPin1,HIGH);
-    digitalWrite(motorPin2,LOW);
-  } else {
+  if ((isLeft==0 && iscenter==0 && isRight==1)||(isLeft==0 && iscenter==1 && isRight==1)) {
+     Serial.println("Left");
+    digitalWrite(motorPin1,0);
+     digitalWrite(motorPin2,1);
+  }
+  
+  else if ((isLeft==1 && iscenter==0 && isRight==0)||(isLeft==1 && iscenter==1 && isRight==0)) {
+  
+Serial.println("Right");
+  digitalWrite(motorPin1,1);
+     digitalWrite(motorPin2,0);
+     
+  } 
+  
+      else if((isLeft==1 && iscenter==0 && isRight==1)){
     Serial.println("Straight");
-    digitalWrite(motorPin3,HIGH);
-     digitalWrite(motorPin4,LOW);
-       digitalWrite(motorPin1,HIGH);
-     digitalWrite(motorPin4,LOW);
+    digitalWrite(motorPin1,1);
+     digitalWrite(motorPin2,1);
     
   }
  }
-  delay(1000); 
 }
